15c5d3c1a29b26913d380fbc83f83b64c7168656
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-04-15 12:32:54 -0400

    updated demo day with routes of the index into the lab report page

--- app/routes.js
+++ app/routes.js
@@ -1,149 +1,148 @@
 module.exports = function(app, passport, db) {
-const ObjectID = require('mongodb').ObjectID
-app.get('/draw', )
-// find one thats rendereing a template and start there
+  const ObjectID = require('mongodb').ObjectID
+  app.get('/draw', )
+  // find one thats rendereing a template and start there
 
-// create another route for API to retreive a compound
-// put the object here, so itll be a JS object
-// endpoint thats starting the data --> creating a route that the API is sending a request to and thats where it gets the data
+  // create another route for API to retreive a compound
+  // put the object here, so itll be a JS object
+  // endpoint thats starting the data --> creating a route that the API is sending a request to and thats where it gets the data
 
 
-// normal routes ===============================================================
+  // normal routes ===============================================================
 
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
+  // show the home page (will also have our login links)
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
 
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-        db.collection('messages').find().toArray((err, result) => { //callback function
-          if (err) return console.log(err)
-          res.render('profile.ejs', {
-            user : req.user, // this is the data object we are passing to the template
-            messages: result // array of messages that we are looking in the database
-          })
-        })
-    });
-
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-
-// message board routes ===============================================================
-
-    app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/profile')
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function(req, res) {
+    db.collection('messages').find().toArray((err, result) => { //callback function
+      if (err) return console.log(err)
+      res.render('profile.ejs', {
+        user : req.user, // this is the data object we are passing to the template
+        messages: result // array of messages that we are looking in the database
       })
     })
+  });
 
-    app.put('/messages', (req, res) => {
-      db.collection('messages')
-      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-        $set: {
-          thumbUp:req.body.thumbUp + 1
-        }
-      }, {
-        sort: {_id: -1},
-        upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
-      })
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // message board routes ===============================================================
+
+  app.post('/messages', (req, res) => {
+    db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/profile')
     })
+  })
 
-    app.put('/thumbDown', (req, res) => {
-      db.collection('messages')
-      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-        $set: {
-          thumbUp:req.body.thumbUp - 1
-        }
-      }, {
-        sort: {_id: -1},
-        upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
-      })
+  app.put('/messages', (req, res) => {
+    db.collection('messages')
+    .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+      $set: {
+        thumbUp:req.body.thumbUp + 1
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
     })
+  })
 
-    app.delete('/messages', (req, res) => {
-      db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-        if (err) return res.send(500, err)
-        res.send('Message deleted!')
-      })
+
+  app.delete('/messages', (req, res) => {
+    db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
     })
+  })
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
 
-    // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function(req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
 
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function(req, res) {
+    var user            = req.user;
+    user.local.email    = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
     });
+  });
 
-// routes for each lab
-app.get('/lab/:id', function(req, res) {
-  const id = req.params.id //got the id off the request
-  db.collection('messages').findOne( // gave us all the data that the ID refers to
-    { _id: new ObjectID(id) } // object literals
-  ).then((result) => {    //write .then after you do the function call .then is also a function
-        console.log(result)
-    res.render('lab.ejs', { lab: result}) //return the property from the fineOne function LAB INFO GOT SWITCHED TO RESULT BC OF LINE 135
+  // routes for each lab
+  app.get('/lab/:id', function(req, res) {
+    const id = req.params.id //got the id off the request
+    db.collection('messages').findOne( // gave us all the data that the ID refers to
+      { _id: new ObjectID(id) } // object literals
+    ).then((result) => {    //write .then after you do the function call .then is also a function
+      console.log(result)
+      res.render('lab.ejs', { lab: result}) //return the property from the fineOne function LAB INFO GOT SWITCHED TO RESULT BC OF LINE 135
 
-  }).catch (err => console.log(err))
-});
+    }).catch (err => console.log(err))
+  });
+
+
+  app.put('/lab', (req, res) => {
+    db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, { //find the one by searching for the ID; req.body.id --> when we call the API, the molecule.js, whatever we pass in as a body is going to be like an object it hsould have the idea and thats where the request comes from
+      $set: {
+        smiles:req.body.smiles // same as line 131, basically an object like id and smart. search forr rdb entrey and smart to update database entry
+      }
+    }, (err, result) => {
+      console.log(result)
+      if (err) return res.send(err)
+      res.send(result)
+    })
+  })
 }
-
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated())
+  return next();
 
-    res.redirect('/');
+  res.redirect('/');
 }
--- public/molecule.js
+++ public/molecule.js
@@ -1,33 +1,65 @@
 // const initialMolecule = "<%= lab.molecule =>"   this is how you will pass th emolecule the database to the server to ketcher
-         var initialMolecule = [
-        "",
-        "  Ketcher 02151213522D 1   1.00000     0.00000     0",
-        "",
-        "  6  6  0     0  0            999 V2000",
-        "   -1.1750    1.7500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
-        "   -0.3090    1.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
-        "   -0.3090    0.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
-        "   -1.1750   -0.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
-        "   -2.0410    0.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
-        "   -2.0410    1.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
-        "  1  2  1  0     0  0",
-        "  2  3  2  0     0  0",
-        "  3  4  1  0     0  0",
-        "  4  5  2  0     0  0",
-        "  5  6  1  0     0  0",
-        "  6  1  2  0     0  0",
-        "M  END"
-      ].join("\n"); // one big single string, store it as a single string in mongo db
-    const ketcherFrame = document.getElementById("ketcher-frame"); // line 46 ID -- talk to ketcheeer. talk to ketcher frame ewith the ID
-    console.log("ketcherFrame:", ketcherFrame);
-    ketcherFrame.addEventListener('load', function () { // waiting for the browser to load HTML; so its waiting for the second html page to load
-      const ketcher = this.contentWindow.ketcher; //ketcher is equal to ketcher object inside that iframe
-      ketcher.setMolecule(initialMolecule);
-      const getButton = document.getElementById("getMolecule")
-      getButton.addEventListener("click",
-        function (){
-          const molecule = ketcher.getSmiles()
-          console.log(molecule)
-        }
-    )
-    });
+//    var initialMolecule = [
+//   "",
+//   "  Ketcher 02151213522D 1   1.00000     0.00000     0",
+//   "",
+//   "  6  6  0     0  0            999 V2000",
+//   "   -1.1750    1.7500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
+//   "   -0.3090    1.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
+//   "   -0.3090    0.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
+//   "   -1.1750   -0.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
+//   "   -2.0410    0.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
+//   "   -2.0410    1.2500    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0",
+//   "  1  2  1  0     0  0",
+//   "  2  3  2  0     0  0",
+//   "  3  4  1  0     0  0",
+//   "  4  5  2  0     0  0",
+//   "  5  6  1  0     0  0",
+//   "  6  1  2  0     0  0",
+//   "M  END"
+// ].join("\n"); // one big single string, store it as a single string in mongo db
+
+
+var ketcher;
+const ketcherFrame = document.getElementById("ketcher-frame"); // line 46 ID -- talk to ketcheeer. talk to ketcher frame ewith the ID
+console.log("ketcherFrame:", ketcherFrame);
+ketcherFrame.addEventListener('load', function () { // waiting for the browser to load HTML; so its waiting for the second html page to load
+  ketcher = this.contentWindow.ketcher; //ketcher is equal to ketcher object inside that iframe
+  // ketcher.setMolecule(initialMolecule);
+  if (lab.smiles){
+    ketcher.setMolecule(lab.smiles)
+  }
+  console.log(lab)
+});
+
+const saveButton = document.getElementById("saveMolecule");
+saveButton.addEventListener('click', function(){
+  const smiles = ketcher.getSmiles();
+  const id = lab._id;
+  fetch('/lab', {
+    method: 'PUT',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'smiles': smiles,
+      'name': lab.name,
+      'msg': lab.msg
+
+    })
+  })
+
+
+  // fetch('messages', {
+  //   method: 'delete',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({
+  //     'name': name,
+  //     'msg': msg
+  //   })
+  // }).then(function (response) {
+  //   window.location.reload()
+  // })
+});
